97d492bf04fc8977b8c9f8be5eef9583b0cfb4f7
xdxdhh <xdxdhh@debian-epyc.lan>
2024-02-09 09:09:02 +0100

    init

---
+++ frontend/src/main.ts
@@ -0,0 +1,27 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+  
+
+const app = createApp(App)
+
+app.use(router)
+app.use(vuetify)
+
+
+app.mount('#app')
