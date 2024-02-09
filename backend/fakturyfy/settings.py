5e0d51f921205d10e0eee43a702701a48c88387c
xdxdhh <d.varsikova@email.cz>
2024-02-09 17:20:43 +0100

    split of form into new component

--- backend/fakturyfy/settings.py
+++ backend/fakturyfy/settings.py
@@ -130,3 +130,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True #delete in prod
