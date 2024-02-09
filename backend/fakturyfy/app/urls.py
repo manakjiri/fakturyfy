55eacd0652fa780aa6d2fe7a7865a4615624641c
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-02-09 18:38:39 +0100

    invoince list get api works

--- backend/fakturyfy/app/urls.py
+++ backend/fakturyfy/app/urls.py
@@ -7,4 +7,5 @@ router.register(r'entity', views.EntityViewSet, basename='entity')
 
 urlpatterns = [
     path('invoice/new', views.NewInvoice.as_view(), name='new_invoice'),
+    path('invoice/list/<int:provider>/<int:client>', views.ListInvoices.as_view(), name='list_invoices')
 ] + router.urls
