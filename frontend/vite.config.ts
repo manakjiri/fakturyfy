713f595c6d6614d3d814fc18b77eafa06fbc7c08
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-02-09 09:31:50 +0100

    vue build wip

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build : {
+    outDir: '../backend/faturyfy/apps/core/templates/core',
+    assetsDir: 'static'
+  },
 })
