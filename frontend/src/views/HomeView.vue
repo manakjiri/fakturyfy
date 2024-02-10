e5affa4d02a95cc0c820064d59262c5c307d2559
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-02-10 11:39:48 +0100

    sort of ok styling of the invoice table

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,38 +10,43 @@
     </v-row>
   </v-container>
 
-  <v-container v-if="faktury"> 
-    <v-row >
+  <v-container v-if="faktury">
+    <v-row>
       <v-col cols="auto">
-         <v-select class="selector" label="Poskytovatel" v-model="provider_id" :items="entities" item-title="abbreviation" item-value="id"></v-select>
+        <v-select class="selector" label="Poskytovatel" v-model="provider_id" :items="entities" item-title="abbreviation"
+          item-value="id"></v-select>
       </v-col>
       <v-col cols="auto">
-         <v-select class="selector" label="Odběratel" v-model="client_id" :items="entities" item-title="abbreviation" item-value="id"></v-select>
+        <v-select class="selector" label="Odběratel" v-model="client_id" :items="entities" item-title="abbreviation"
+          item-value="id"></v-select>
       </v-col>
       <v-spacer></v-spacer>
       <v-col cols="auto">
         <v-btn @click="showFakturaForm = true">Nová faktura</v-btn>
       </v-col>
     </v-row>
-    
-    <v-row v-for="invoice in existing_invoices" :key="invoice.name">
-      <v-col cols="12" sm="1" justify="left"><v-icon icon="mdi-file-pdf-box"></v-icon></v-col>
-      <v-col cols="12" sm="5" justify="left"> {{ invoice.name }} </v-col>
-      <v-col cols="12" sm="4" justify="left"> {{ new Date(invoice.modify_time*1000).toLocaleString('cs') }} </v-col>
-      <v-col cols="12" sm="2" justify="right">
+
+    <v-container>
+      <v-row v-for="invoice in existing_invoices" :key="invoice.name" justify="space-between">
+        <v-col cols="9" align-self="center">
+          <v-row no-gutters justify="start">
+            <v-col cols="2"><v-icon icon="mdi-file-pdf-box"></v-icon></v-col>
+            <v-col><v-sheet>{{ invoice.name }}</v-sheet></v-col>
+            <v-col><v-sheet>{{ new Date(invoice.modify_time * 1000).toLocaleString('cs') }}</v-sheet></v-col>
+          </v-row>
+        </v-col>
+        <v-col><v-spacer></v-spacer></v-col>
+        <v-col cols="2" align-self="center">
           <v-btn :href="`static/${invoice.path}`" target="_blank">otevřít</v-btn>
-      </v-col>
-      <v-divider></v-divider>
-    </v-row>
+        </v-col>
+        <v-divider></v-divider>
+      </v-row>
+    </v-container>
   </v-container>
 
   <v-container v-if="subjekty">
     Subjekty:
-    <v-data-table
-      :headers="headers"
-      :items="entities"
-      items-per-page-text="Subjektů na stránku"
-    >
+    <v-data-table :headers="headers" :items="entities" items-per-page-text="Subjektů na stránku">
       <template v-slot:item.edit="{ item }">
         <v-icon icon="mdi-magnify-expand"></v-icon>
       </template>
@@ -65,7 +70,6 @@
       <FakturaForm></FakturaForm>
     </v-dialog>
   </v-container>
-
 </template>
 
 <script setup lang="ts">
@@ -73,8 +77,9 @@ import { onMounted, ref, watch } from "vue";
 import EntityForm from "@/components/EntityForm.vue";
 import FakturaForm from "@/components/FakturaForm.vue";
 import useFetching from "@/js/useFetching";
+import { assert } from "console";
 
-const {Axios} = useFetching();
+const { Axios } = useFetching();
 
 const showForm = ref(false);
 const showFakturaForm = ref(false);
@@ -99,7 +104,11 @@ interface InvoiceDescription {
 
 const client_id = ref<number | null>(null);
 const provider_id = ref<number | null>(null);
-const existing_invoices = ref<Array<InvoiceDescription>>([]);
+const existing_invoices = ref<Array<InvoiceDescription>>([
+  { path: "path", name: "name", modify_time: 0 },
+  { path: "path", name: "name", modify_time: 1000000000 },
+  { path: "path", name: "name", modify_time: 10000000000 },
+]);
 
 watch([client_id, provider_id], async () => {
   if (client_id.value && provider_id.value) {
@@ -149,5 +158,4 @@ onMounted(async () => {
 .selector {
   width: 200px;
 }
-
 </style>
