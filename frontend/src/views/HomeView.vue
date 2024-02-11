6e372cdc4f860a40f3fdb2c9913db1b4d8bc99f7
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-02-11 14:53:35 +0100

    transition between entities and invoices

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,138 +1,163 @@
 <template>
-  <v-container>
-    <v-row justify="space-between">
-      <v-col cols="auto">
-        <v-btn class="my-button" @click="fakturyView">Faktury</v-btn>
-      </v-col>
-      <v-col cols="auto">
-        <v-btn class="my-button" @click="subjektyView">Subjekty</v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
-
-  <v-container v-if="faktury">
-    <v-sheet
-      class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4"
-      height="250"
-      max-width="800"
-      width="100%"
-      color="rgba(0, 0, 0, 0)"
-    >
-      <v-row justify="space-between" align="start">
-        <v-col cols="3">
-          <v-select
-            class="selector"
-            label="Poskytovatel"
-            :append-icon="'mdi-close'"
-            @click:append="clearSelectionProvider"
-            v-model="provider_id"
-            :items="entities"
-            item-title="abbreviation"
-            item-value="id"
-          ></v-select>
-        </v-col>
-        <v-col cols="3">
-          <v-select
-            class="selector"
-            label="Odběratel"
-            :append-icon="'mdi-close'"
-            @click:append="clearSelectionClient"
-            v-model="client_id"
-            :items="entities"
-            item-title="abbreviation"
-            item-value="id"
-          ></v-select>
-        </v-col>
-        <v-col cols="3">
-          <v-btn
-            class="my-button"
-            size="x-large"
-            style="height: 57px;"
-            @click="showFakturaForm = true"
-            :disabled="providerClientNotSelected"
-            >Nová faktura</v-btn
-          >
-        </v-col>
-      </v-row>
-    </v-sheet>
-
-    <v-card class="my-card">
-      <v-container v-if="existing_invoices">
-        <v-row
-          v-for="invoice in existing_invoices"
-          :key="invoice.name"
-          justify="space-between"
-        >
-          <v-col cols="9" align-self="center">
-            <v-row no-gutters justify="start">
-              <v-col cols="1"><v-icon icon="mdi-file-pdf-box"></v-icon></v-col>
-              <v-col
-                ><v-sheet class="my-sheet">{{ invoice.name }}</v-sheet></v-col
-              >
-              <v-col
-                ><v-sheet class="my-sheet"
-                  >{{ invoice.provider }} → {{ invoice.client }}</v-sheet
-                ></v-col
-              >
-              <v-col
-                ><v-sheet class="my-sheet">{{
-                  new Date(invoice.modify_time * 1000).toLocaleString("cs")
-                }}</v-sheet></v-col
-              >
-            </v-row>
-          </v-col>
-          <v-col><v-spacer></v-spacer></v-col>
-          <v-col cols="2" align-self="center">
+  <v-slide-x-transition>
+    <v-container v-show="faktury">
+      <v-container>
+        <v-row justify="end">
+          <v-col cols="auto">
             <v-btn
-              class="my-button"
-              :href="`static/${invoice.path}`"
-              target="_blank"
-              >otevřít</v-btn
+              variant="text"
+              append-icon="mdi-arrow-right"
+              size="x-large"
+              @click="subjektyView"
+              >Subjekty</v-btn
             >
           </v-col>
-          <v-divider></v-divider>
         </v-row>
       </v-container>
-    </v-card>
-  </v-container>
-
-  <v-container v-if="subjekty">
-    <v-row class="text-h5 mt-2 mb-2 font-weight-light"> Subjekty: </v-row>
-    <v-row>
-      <v-data-table
-        class="my-data-table elevation-2"
-        :headers="headers"
-        :items="entities"
-        items-per-page-text="Subjektů na stránku"
+      <v-sheet
+        class="d-flex align-center justify-center flex-wrap text-center mx-auto px-4"
+        height="250"
+        max-width="800"
+        width="100%"
+        color="rgba(0, 0, 0, 0)"
       >
-        <template v-slot:item.edit="{ item }">
-          <v-icon @click="editEntity(item)" icon="mdi-magnify-expand"></v-icon>
-        </template>
-      </v-data-table>
-    </v-row>
+        <v-row justify="space-between" align="start">
+          <v-col cols="3">
+            <v-select
+              class="selector"
+              label="Dodavatel"
+              :append-icon="'mdi-close'"
+              @click:append="clearSelectionProvider"
+              v-model="provider_id"
+              :items="entities"
+              item-title="abbreviation"
+              item-value="id"
+            ></v-select>
+          </v-col>
+          <v-col cols="3">
+            <v-select
+              class="selector"
+              label="Odběratel"
+              :append-icon="'mdi-close'"
+              @click:append="clearSelectionClient"
+              v-model="client_id"
+              :items="entities"
+              item-title="abbreviation"
+              item-value="id"
+            ></v-select>
+          </v-col>
+          <v-col cols="3">
+            <v-btn
+              class="my-button"
+              size="x-large"
+              style="height: 57px"
+              @click="showFakturaForm = true"
+              :disabled="providerClientNotSelected"
+              >Nová faktura</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-sheet>
 
-    <v-row justify="space-between">
-      <v-col cols="auto"> </v-col>
-      <v-col cols="auto">
-        <v-btn class="my-button" @click="createNewEntity">Nový subjekt</v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+      <v-card class="my-card">
+        <v-container v-if="existing_invoices">
+          <v-row
+            v-for="invoice in existing_invoices"
+            :key="invoice.name"
+            justify="space-between"
+          >
+            <v-col cols="9" align-self="center">
+              <v-row no-gutters justify="start">
+                <v-col cols="1"
+                  ><v-icon icon="mdi-file-pdf-box"></v-icon
+                ></v-col>
+                <v-col
+                  ><v-sheet class="my-sheet">{{ invoice.name }}</v-sheet></v-col
+                >
+                <v-col
+                  ><v-sheet class="my-sheet"
+                    >{{ invoice.provider }} → {{ invoice.client }}</v-sheet
+                  ></v-col
+                >
+                <v-col
+                  ><v-sheet class="my-sheet">{{
+                    new Date(invoice.modify_time * 1000).toLocaleString("cs")
+                  }}</v-sheet></v-col
+                >
+              </v-row>
+            </v-col>
+            <v-col><v-spacer></v-spacer></v-col>
+            <v-col cols="2" align-self="center">
+              <v-btn
+                class="my-button"
+                :href="`static/${invoice.path}`"
+                target="_blank"
+                >otevřít</v-btn
+              >
+            </v-col>
+            <v-divider></v-divider>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-container>
+  </v-slide-x-transition>
 
-  <v-container>
-    <v-dialog v-model="showForm" scrim="true" class="form">
-      <EntityForm :entity_id="chosenEntityId"></EntityForm>
-    </v-dialog>
-  </v-container>
+  <v-slide-x-reverse-transition>
+    <v-container v-show="subjekty">
+      <v-container>
+        <v-row justify="start">
+          <v-col cols="auto">
+            <v-btn
+              variant="text"
+              prepend-icon="mdi-arrow-left"
+              size="x-large"
+              @click="fakturyView"
+              >Faktury</v-btn
+            >
+          </v-col>
+        </v-row>
+      </v-container>
 
-  <v-container>
-    <v-dialog v-model="showFakturaForm" scrim="true" class="form">
-      <FakturaForm
-        :client_id="client_id"
-        :provider_id="provider_id"
-      ></FakturaForm>
-    </v-dialog>
-  </v-container>
+      <v-row class="text-h5 mt-15 mb-5 font-weight-light">Uložené subjekty</v-row>
+      <v-row>
+        <v-data-table
+          class="my-data-table elevation-2"
+          :headers="headers"
+          :items="entities"
+          items-per-page-text="Subjektů na stránku"
+        >
+          <template v-slot:item.edit="{ item }">
+            <v-icon
+              @click="editEntity(item)"
+              icon="mdi-magnify-expand"
+            ></v-icon>
+          </template>
+        </v-data-table>
+      </v-row>
+
+      <v-row justify="space-between">
+        <v-col cols="auto"> </v-col>
+        <v-col cols="auto">
+          <v-btn class="my-button" @click="createNewEntity">Nový subjekt</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container>
+      <v-dialog v-model="showForm" scrim="true" class="form">
+        <EntityForm :entity_id="chosenEntityId"></EntityForm>
+      </v-dialog>
+    </v-container>
+
+    <v-container>
+      <v-dialog v-model="showFakturaForm" scrim="true" class="form">
+        <FakturaForm
+          :client_id="client_id"
+          :provider_id="provider_id"
+        ></FakturaForm>
+      </v-dialog>
+    </v-container>
+  </v-slide-x-reverse-transition>
 </template>
 
 <script setup lang="ts">
@@ -149,13 +174,17 @@ const faktury = ref(true);
 const subjekty = ref(false);
 
 function fakturyView() {
-  faktury.value = true;
   subjekty.value = false;
+  setTimeout(() => {
+    faktury.value = true;
+  }, 250);
 }
 
 function subjektyView() {
   faktury.value = false;
-  subjekty.value = true;
+  setTimeout(() => {
+    subjekty.value = true;
+  }, 250);
 }
 
 interface InvoiceDescription {
